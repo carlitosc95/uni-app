d55d00434ef522d99d2167238b9c4e2a84766034
charly_pineapple <kuro.jg@gmail.com>
2023-06-09 17:57:16 -0600

    feat: navbar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,8 @@
+function NavBar() {
+  return (
+    <div className="navbar bg-base-100 border-r-2 sticky top-0">
+      <a className="btn btn-ghost normal-case text-2xl">UniFinder</a>
+    </div>
+  );
+}
+export default NavBar;
