eec7ee31124978e1defe341e23ba03bdf96b8f51
charly_pineapple <kuro.jg@gmail.com>
2023-06-09 18:01:01 -0600

    feat: types for global use

---
+++ src/types.tsx
@@ -0,0 +1,22 @@
+interface UniversityData {
+  country: String;
+  alpha_two_code: String;
+  name: String;
+  web_pages: Array<String>;
+}
+
+interface ListProps {
+  items: UniversityData[];
+  selected: Number;
+}
+interface QueryData {
+  uni_name: string;
+  country: string;
+  limit: number;
+}
+interface InputProps {
+  placeholder: string;
+  onChangeCallback: ({ data, key }) => void;
+  name: string;
+}
+export { UniversityData, ListProps, QueryData, InputProps };
